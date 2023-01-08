4ac1ebba914bd9435864585a33030ac4e74fd5a7
sathish-radan <narwalsathish@gmail.com>
2023-01-08 23:44:42 +0530

    Day2 challenge Tip calculator

---
+++ Day-2 Tip Calcluator/tipCalculator.py
@@ -0,0 +1,10 @@
+print("Welcome to the tip calculator")
+
+bill = float(input("What was the total bill ? $"))
+
+tip = float(input("What percentage tip would you like to give? 10, 12 or 15 ? $"))
+
+split_by_people = float(input("How many perople to split the bill ? "))
+
+
+print("Each person should pay $",(bill * (tip/100) + bill)/split_by_people)
