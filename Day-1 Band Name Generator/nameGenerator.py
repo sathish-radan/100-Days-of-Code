667254e5e4a95274e5b48b3c5c99c4ecd826fce4
sathish-radan <narwalsathish@gmail.com>
2023-01-08 21:37:18 +0530

    Day1 challenge

---
+++ Day-1 Band Name Generator/nameGenerator.py
@@ -0,0 +1,7 @@
+print("Welcome to the Band Name Generator !")
+
+country_name = input("what's name of the city you grew up ?\n").strip()
+pet_name = input("what was your's pet name ? \n").strip()
+
+print("your Band Name could be " + country_name +" "+ pet_name)
+
