0d2af23241d75bf19dddc5e0fe5d6caa1c076141
sathish-radan <narwalsathish@gmail.com>
2023-01-10 18:58:07 +0530

    Day3 Challenge Treasure Island

--- Day-2 Tip Calcluator/tipCalculator.py
+++ Day-2 Tip Calcluator/tipCalculator.py
@@ -7,4 +7,4 @@ tip = float(input("What percentage tip would you like to give? 10, 12 or 15 ? $"
 split_by_people = float(input("How many perople to split the bill ? "))
 
 
-print("Each person should pay $",(bill * (tip/100) + bill)/split_by_people)
+print("Each person should pay $",(bill * (tip/100) + bill)/split_by_people)t
